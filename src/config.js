510a3b8a89412d86fb818777a0c18f24ad74c394
samerfouad <samerfouad@outlook.com>
2020-04-12 19:59:45 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 export const LOGIN_URL = 'http://173.193.99.222:31023';
-export const LOAN_SERVICE_URL = 'http://184.172.233.23:31833';
+export const LOAN_SERVICE_URL = 'http://173.193.99.222:31301';
